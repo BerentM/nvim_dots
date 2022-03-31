021b4bfe3a393d50fe2b0a55efeb952ae6f1bc59
Mateusz <>
2022-03-31 20:16:13 +0200

    Lua LSP + stylua

--- lua/conf/lsp.lua
+++ lua/conf/lsp.lua
@@ -21,7 +21,7 @@ local on_attach = function(client)
 	vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
 end
 
-local servers = {"pyright", "gopls"}
+local servers = {"pyright", "gopls", "sumneko_lua"}
 for _, lsp in pairs(servers) do
 	require'lspconfig'[lsp].setup{
 		capabilities=capabilities,
--- lua/conf/null-ls.lua
+++ lua/conf/null-ls.lua
@@ -4,11 +4,14 @@ null_ls.setup({
     sources = {
         -- python dependencies
         -- pip install mypy yapf pydocstyle vulture
+        -- scoop installs
+        -- scoop install stylua
         null_ls.builtins.formatting.goimports,
         null_ls.builtins.formatting.gofmt,
         null_ls.builtins.diagnostics.mypy, -- static code check
         null_ls.builtins.formatting.yapf, -- code formatting
         null_ls.builtins.diagnostics.pydocstyle, -- docstring check
-        null_ls.builtins.diagnostics.vulture, -- look for unused code
+        -- null_ls.builtins.diagnostics.vulture, -- look for unused code
+        null_ls.builtins.formatting.stylua, -- lua formatting
     },
 })
